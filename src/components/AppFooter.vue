e8e3f3c0a0a39137c3d30bace15858d908d9dc9f
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-03-21 16:25:10 +0100

    add components: AppHeader, AppMain, AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+
+    <!-- /#site_footer -->
+    <footer id="site_footer">
+        <div class="footer_container">
+            <div class="info_footer">
+                <h2>Boolando s.r.l.</h2>
+                <aside class="info_footer">
+                    <span>Informazioni legali</span>
+                    <span>Informativa sulla privacy</span>
+                    <span>Diritto di recesso</span>
+                </aside>
+
+            </div>
+            <div class="social">
+                <h6>Trovaci anche su:</h6>
+                <aside class="social">
+                    <img src="/public/img/svg-path.svg" alt="">
+                    <img src="/public/img/square-facebook-2.svg" alt="">
+                    <img src="/public/img/square-instagram-2.svg" alt="">
+                    <img src="/public/img/square-pinterest-2.svg" alt="">
+                    <img src="/public/img/square-youtube-2.svg" alt="">
+                </aside>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <!-- /#site_header -->
+    <header id="site_header">
+        <!-- /#target -->
+        <div class="target">
+            <ul class="target">
+                <li>Donna</li>
+                <li>Uomo</li>
+                <li>Bambini</li>
+            </ul>
+        </div>
+        <!-- /#logo -->
+        <div id="logo">
+            <img src="/public/img/boolean-logo.png" alt="">
+        </div>
+        <!-- /#call_to_action -->
+        <div id="call_to_action">
+            <img src="/public/img/user-regular-2.svg" alt="">
+            <img src="/public/img/heart-regular-2.svg" alt="">
+            <img src="/public/img/bag-shopping-solid-2.svg" alt="">
+        </div>
+    </header>
+</template>
+
+<style scoped></style>
