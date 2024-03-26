ebd19d0452db380d079b6c9b1ba8a6ca90cb4ad7
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-03-26 18:00:34 +0100

    fix hover heart issue

--- src/components/MyModal.vue
+++ src/components/MyModal.vue
@@ -7,7 +7,7 @@ export default {
     ],
     data() {
         return {
-
+            hoverHeart: false
         }
     },
     methods: {
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -36,43 +36,8 @@ export default {
 
 <template>
 
-    <!-- <div v-if="open" class="card_modal d-flex" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
-        <div class="image_box ">
-            <img class="frontMod mt-2" :src="'/img/' + frontImage" alt="">
-            <img class="backMod mt-2" :src="'/img/' + backImage" alt="">
-        </div>
-
-        <div class="textMod">
-
-            <h3 class="nameMod fw-bolder mt-2 text-uppercase" >{{ itemName }}</h3>
-            <h5 class="brandMod">{{ brand }}</h5>
-            
-            <div @click="like()" :class="{ active: hoverHeart }" class="hearts">
-                <div class="like modalLike"> Add to wishlist &hearts;</div>
-            </div>           
-            <div class="like cart"> Add to the cart 🛒 </div>
-
-            <div class="descriptionTextMod">
-                <p>{{ description }}</p>
-            </div>
-            
-            <div class="priceMod ms-1">
-                <span class="newpriceMod fw-bold" v-if="badges[badges.length - 1].type == 'discount'">{{ (price /
-                100 * (100 - Math.abs((parseInt(badges[badges.length - 1].value))))).toFixed(2) + '&euro;' }}</span>
-
-                <span
-                :class="{ 'noDiscount': badges[badges.length - 1].value != 'Sostenibilità' && badges[badges.length - 1].value != null }"
-                class="oldprice ms-1">{{ price + '&euro;'}}
-                </span>
-            </div>
-
-        </div>
-
-        <button aria-label="Close modal" class="modal_btn" @click="open = false">X</button>
-    </div> -->
-
     <div class="col mt-1 mb-4 ">
-        <div @mouseover="hover = true" @mouseleave="hover = false" @click="$emit('show-modal', product)" class="card h-100">
+        <div @mouseover="hover = true" @mouseleave="hover = false"  class="card h-100">
 
             <div @click="like()" :class="{ active: hoverHeart }" class="hearts">
                 <div class="like">&hearts;</div>
@@ -89,7 +54,7 @@ export default {
             <img v-else :src="'/img/' + frontImage" class="front model w-100" alt="">
 
             <div class="brand ms-1">{{ brand }}</div>
-            <div @click="open = true" class="description fw-bold text-uppercase ms-1">{{ product.itemName }}</div>
+            <div @click="$emit('show-modal', product)" class="description fw-bold text-uppercase ms-1">{{ product.itemName }}</div>
 
             <div class="price ms-1">
                 <span class="newprice fw-bold fs-6 me-2" v-if="badges[badges.length - 1].type == 'discount'">{{ (price /
