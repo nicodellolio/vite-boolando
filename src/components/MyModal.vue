2ee5a7ebea74c1509a2da039d67497272ff10f7c
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-09-01 15:13:21 +0200

    add images to the daily activities

--- src/components/MyModal.vue
+++ src/components/MyModal.vue
@@ -26,7 +26,9 @@ export default {
     <div v-if="open" class="card_modal d-flex" role="dialog" aria-labelledby="modalTitle"
         aria-describedby="modalDescription">
         <div class="image_box ">
-            <img class="frontMod mt-2 h-100"  :src="'/img/' + day.frontImage" alt="">
+            <img class="top_img mt-2 h-100"  :src="'/img/' + day.stops[0].singleImage" alt="">
+            <img class="mt-2 h-100"  :src="'/img/' + day.stops[1].singleImage" alt="">
+            <img class="bott_img mt-2 h-100"  :src="'/img/' + day.stops[2].singleImage" alt="">
         </div>
 
         <div class="textMod">
@@ -39,7 +41,7 @@ export default {
 
             <div class="descriptionTextMod">
                 
-                <ul class="mb-3">
+                <ul class="mb-5">
                     <li>
                         <h3>Stop n.1</h3>
                         <h1 class="fw-bold mb-3">{{ day.stops[0].name }}</h1>
@@ -64,13 +66,18 @@ export default {
                     day of our trip sees us go right to {{ day.stops[0].name }} famous for the unique {{ day.stops[0].note }} to spend half of our day
                     and stop for lunch nearby. Then we'll take a walk or rent a bike to {{ day.stops[1].name }}.
                     Eventually right before dinner our last stop for the day is going to be {{ day.stops[2].name }} to make sure not to miss the amazing {{ day.stops[2].note }}.
-                    The accomodation for the night will be at {{ day.accomodation }}. The expected expenses for our
+                    The accomodation for the night will be at {{ day.accomodation }}. 
+                    
+                </p>
+                <h3 class="mt-5">
+                    The expected expenses for our
                     <span v-if="day.id == 1">first </span>
                     <span v-else-if="day.id == 2">second </span>
                     <span v-else-if="day.id == 3">third </span>
                     <span v-else-if="day.id > 3">{{ day.id }}th </span>
-                    day of trip are around ${{ day.expected_expenses }}.
-                </p>
+                    day of trip are around 
+                    <span class="text-success fs-1 ms-2 fw-bold">${{ day.expected_expenses }}</span>
+                </h3>
             </div>
 
 
@@ -86,4 +93,16 @@ export default {
 
 <style scoped lang='scss'>
 @import '../assets/scss/partials/common_rules';
-// @import '../assets/scss/partials/main.scss';</style>
+// @import '../assets/scss/partials/main.scss';
+
+.descriptionTextMod p, span, h3{
+    font-size: 2rem;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.image_box{
+    height: fit-content;
+    padding-top: 3rem;
+}
+
+</style>
